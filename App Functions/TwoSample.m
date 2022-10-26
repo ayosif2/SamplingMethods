e1cc6f55151015b089ddad9951fc8eb62deef67a
ayosif2 <ayosif2@gmail.com>
2022-10-26 18:02:45 +0200

    V1.5
    a huge update made by me read update notes in releases for the changes

--- App Functions/TwoSample.m
+++ App Functions/TwoSample.m
@@ -2,11 +2,16 @@ function [Y,angle] = TwoSample(k,freq0,Tsample)
 %TwoSample This function applies the three sample predictive alghorithm
 %to any given input data k, freq0 is the model frequency, while Tsample is
 %the sampling time
-	Yc=k;
-	Ys(1)=(Yc(1)*cos(2*freq0*pi*Tsample)-0)/sin(2*freq0*pi*Tsample);
-    for j=2:length(k)
+	Yc=k;                   %in this alghorism Yc are always equal to k
+    Ys=zeros(1,length(k));  %initiating the size of the matrix to improve performance
+	Ys(1)=(Yc(1)*cos(2*freq0*pi*Tsample)-0)/sin(2*freq0*pi*Tsample);    %doing the first element which is faulty in this alghorism
+    Y=zeros(1,length(k));   %initiating the size of the matrix to improve performance
+    angle=zeros(1,length(k));   %initiating the size of the matrix to improve performance
+    %applying the alghorism
+    for j=2:length(k)      
         Ys(j)=(Yc(j)*cos(2*freq0*pi*Tsample)-Yc(j-1))/sin(2*freq0*pi*Tsample);
     end
+    %calculating the magnitude and angle
 	for z=1:length(k)
         Y(z)=sqrt(Ys(z)^2+Yc(z)^2);
         angle(z)=atand(Ys(z)/Yc(z));
